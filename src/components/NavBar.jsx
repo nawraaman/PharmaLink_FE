0a2be81d679b4c38bb935a51f52ad24e06db5d23
Zulfa094 <zulfa_94@hotmail.com>
2025-01-06 20:59:33 +0300

    Done from auth

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom'
+
+const NavBar = ({ logOut, user }) => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        {user ? (
+          <>
+            <li>
+              <Link to="/" onClick={logOut}>
+                Log out
+              </Link>
+            </li>
+            <p>Welcome, {user.username}</p>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/auth/signin">Signin</Link>
+            </li>
+            <li>
+              <Link to="/auth/signup">Signup</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ src/pages/auth/Signup.jsx
@@ -0,0 +1,87 @@
+import { signUp } from '../../services/authService'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+const initialFormData = {
+  username: '',
+  password: '',
+  passwordConf: ''
+}
+
+const Signup = ({ getUser }) => {
+  const [message, setMessage] = useState('')
+  const [formData, setFormData] = useState(initialFormData)
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await signUp(formData)
+      await getUser()
+      setFormData(initialFormData)
+      navigate('/')
+    } catch (error) {
+      setMessage(error.response?.data?.error)
+      console.log(error)
+    }
+  }
+
+  const isFormInvalid = () => {
+    return !(
+      formData.username &&
+      formData.password &&
+      formData.password === formData.passwordConf
+    )
+  }
+
+  return (
+    <main>
+      <h1>Sign Up</h1>
+      <p style={{ color: 'red' }}>{message}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="name"
+            value={formData.username}
+            name="username"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={formData.password}
+            name="password"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirm">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm"
+            value={formData.passwordConf}
+            name="passwordConf"
+            onChange={handleChange}
+          />
+        </div>
+        <section>
+          <button disabled={isFormInvalid()}>Sign Up</button>
+          <Link to="/">
+            <button>Cancel</button>
+          </Link>
+        </section>
+      </form>
+    </main>
+  )
+}
+
+export default Signup
