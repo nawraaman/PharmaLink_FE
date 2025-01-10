f4f99e5c8ee3da9721b835885bbb530dee1755ac
nawraaman <nawra.aman55@gmail.com>
2025-01-10 14:56:51 +0300

    delete & update

---
+++ src/pages/Pharmacy/DeletePharmacy.jsx
@@ -0,0 +1,31 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const DeletePharmacy = ({ pharmacies, setPharmacies }) => {
+  let navigate = useNavigate()
+  const { pharmacyId } = useParams()
+
+  const deletePharmacy = async () => {
+    try {
+      await axios.delete(`${BASE_URL}/pharmacy/${pharmacyId}`)
+      const index = pharmacies.findIndex(
+        (pharmacy) => pharmacy._id === pharmacyId
+      )
+      setPharmacies(pharmacies.toSpliced(index, 1))
+      navigate('/pharmacy-list')
+    } catch (error) {
+      console.error('Error deleting pharmacy:', error)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Are you sure you want to delete this pharmacy?</h1>
+      <button onClick={() => navigate(`/pharmacy/${pharmacyId}`)}>No</button>
+      <button onClick={deletePharmacy}>Yes</button>
+    </div>
+  )
+}
+
+export default DeletePharmacy
---
+++ src/pages/Pharmacy/UpdatePharmacy.jsx
@@ -0,0 +1,88 @@
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import axios from 'axios'
+import { BASE_URL } from '../globals'
+
+const UpdatePharmacy = ({ pharmacies, setPharmacies }) => {
+  let navigate = useNavigate()
+  const { pharmacyId } = useParams()
+
+  const [formPharmacy, setFormPharmacy] = useState(null)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/pharmacy/${pharmacyId}`,
+        formPharmacy
+      )
+      const index = pharmacies.findIndex(
+        (pharmacy) => pharmacy._id === pharmacyId
+      )
+      setPharmacies(pharmacies.toSpliced(index, 1, response.data))
+      navigate(`/pharmacy/${pharmacyId}`)
+    } catch (error) {
+      console.error('Error updating pharmacy:', error)
+    }
+  }
+
+  const handleChange = (event) => {
+    setFormPharmacy({ ...formPharmacy, [event.target.id]: event.target.value })
+  }
+
+  useEffect(() => {
+    const getPharmacy = () => {
+      const singlePharmacy = pharmacies.find(
+        (pharmacy) => pharmacy._id === pharmacyId
+      )
+      setFormPharmacy(singlePharmacy)
+    }
+    getPharmacy()
+  }, [pharmacyId, pharmacies])
+
+  return formPharmacy ? (
+    <div>
+      <h1>Update Details for {formPharmacy.name}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          value={formPharmacy.name}
+        />
+
+        <label htmlFor="location">Location:</label>
+        <input
+          type="text"
+          id="location"
+          onChange={handleChange}
+          value={formPharmacy.location}
+        />
+
+        <label htmlFor="noBranches">Number of Branches:</label>
+        <input
+          type="number"
+          id="noBranches"
+          onChange={handleChange}
+          value={formPharmacy.noBranches}
+        />
+
+        <label htmlFor="logo">Logo:</label>
+        <input
+          type="text"
+          id="logo"
+          onChange={handleChange}
+          value={formPharmacy.logo}
+        />
+
+        <button type="submit">Update Pharmacy</button>
+      </form>
+      <button onClick={() => navigate(`/pharmacy/${pharmacyId}`)}>Back</button>
+    </div>
+  ) : (
+    <h1>Loading . . .</h1>
+  )
+}
+
+export default UpdatePharmacy
