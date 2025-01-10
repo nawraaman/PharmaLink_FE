af60aa925a5cd8d3691b84a95339ff5dc9e36516
fatemaalajaimi <fatemaalajaimi@gmail.com>
2025-01-10 10:15:46 +0300

    modify FE for logout/login/signup

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,17 @@
+const Dashboard = ({ user }) => {
+  if (!user) {
+    return <p>Please sign in</p>
+  }
+
+  return (
+    <>
+      <h1>Welcome, {user?.username}</h1>
+      <p>
+        This is the dashboard page where you, and only you, can see a dashboard
+        of all of your things.
+      </p>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Welcome to our website! From here you can view a list of all of the pets
+        we have on site and even add a pet of your own!
+      </p>
+    </div>
+  )
+}
+
+export default Home
